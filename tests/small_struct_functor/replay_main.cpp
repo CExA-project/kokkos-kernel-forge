5ff2f305bc600d6a071995816ca121f844afc123
Trévis Morvany <trevismorvany@gmail.com>
2026-05-20 16:25:09 +0200

    Add tests for replaying struct-based functors

---
+++ tests/small_struct_functor/replay_main.cpp
@@ -0,0 +1,29 @@
+#include <Kokkos_Core.hpp>
+
+#include <kernel_replayer.hpp>
+
+struct Functor {
+ private:
+  int x;
+  int y;
+
+ public:
+  Functor(int x, int y) : x(x), y(y) {}
+
+  KOKKOS_FUNCTION void operator()(int) const {
+    Kokkos::printf("x is %d, y is %d\n", x, y);
+  }
+};
+
+int main(int argc, char* argv[]) {
+  cexa::kernel_replayer::ScopeGuard replay_scope(argc, argv);
+  Kokkos::ScopeGuard kokkos_scope(argc, argv);
+
+  int x = 0;
+  int y = 0;
+  Kokkos::parallel_for("test_kernel", 1,
+                       cexa::kernel_replayer::replay_functor(Functor(x, y)));
+  Kokkos::fence();
+
+  return 0;
+}
---
+++ tests/struct_functor/replay_main.cpp
@@ -0,0 +1,51 @@
+#include <Kokkos_Core.hpp>
+
+#include <kernel_replayer.hpp>
+
+struct Functor {
+ private:
+  int factor;
+  Kokkos::View<int*> values;
+
+ public:
+  Functor(int factor, Kokkos::View<int*> values)
+      : factor(factor), values(values) {}
+
+  KOKKOS_FUNCTION void operator()(int i) const { values(i) *= factor; }
+};
+
+int main(int argc, char* argv[]) {
+  cexa::kernel_replayer::ScopeGuard replay_scope(argc, argv);
+  Kokkos::ScopeGuard kokkos_scope(argc, argv);
+
+  const int N = 1024;
+  Kokkos::View<int*> values("values", 1);
+  // Kokkos::parallel_for(
+  //     "init", values.size(), KOKKOS_LAMBDA(int i) { values(i) = i; });
+
+  int factor = 0;
+  Kokkos::parallel_for(
+      "test_kernel", N,
+      cexa::kernel_replayer::replay_functor(Functor(factor, values)));
+  Kokkos::fence();
+
+  auto replay_values = cexa::kernel_replayer::get_view<int*>("values", N);
+  auto h_replay_values =
+      Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace(), replay_values);
+
+  auto ref_values = cexa::kernel_replayer::get_out_view<int*>("values", N);
+  auto h_ref_values =
+      Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace(), ref_values);
+
+  for (int i = 0; i < N; i++) {
+    if (h_replay_values(i) != h_ref_values(i)) {
+      Kokkos::printf("At index %d, expected %d but got %d\n", i,
+                     h_ref_values(i), h_replay_values(i));
+      return 1;
+    }
+  }
+
+  Kokkos::printf("values(5) = %d\n", h_replay_values(5));
+
+  return 0;
+}
