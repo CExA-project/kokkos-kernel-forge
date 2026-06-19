3413ed8ff99089a7662511c759870ddb3f271c78
Trévis Morvany <trevis.morvany@cea.fr>
2026-06-19 11:17:09 +0200

    Add install tests in the CI

---
+++ tests/install_test/main.cpp
@@ -0,0 +1,23 @@
+#include <Kokkos_Core.hpp>
+
+#include <kernel_extractor.hpp>
+
+int main(int argc, char* argv[]) {
+  Kokkos::ScopeGuard kokkos_scope(argc, argv);
+
+  const int N = 1024;
+  Kokkos::View<int*> values("values", N);
+  Kokkos::parallel_for(
+      "init", values.size(), KOKKOS_LAMBDA(int i) { values(i) = i; });
+
+  Kokkos::parallel_for("test_kernel", N,
+                       cexa::kernel_replayer::replay_functor(
+                           KOKKOS_LAMBDA(int i) { values(i) *= 2; }));
+  Kokkos::fence();
+
+  auto h_values =
+      Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace(), values);
+  Kokkos::printf("values(5) = %d\n", h_values(5));
+
+  return 0;
+}
